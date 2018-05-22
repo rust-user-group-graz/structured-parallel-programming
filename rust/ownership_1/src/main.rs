7ddba0bb3ee1331af5cff396ef2349557b0dad38
davidf <dforsten@gmail.com>
2018-05-22 15:25:44 +0200

    Added some explanations

--- rust/ownership_1/src/main.rs
+++ rust/ownership_1/src/main.rs
@@ -4,7 +4,7 @@
 
 
 
-
+// sole "owner" of v
 fn taking_v(_v: Vec<i32>) {
     //
 }
@@ -13,6 +13,8 @@ fn main() {
     let mut v = Vec::new();
     v.push(1);
     v.push(2);
+
+    // takes ownership of v, which is *not* valid for use in main() after this call
     taking_v(v);
 
     // error, v is not valid at this point
